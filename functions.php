427c9bf40037b2cb7a2d2fb7d849dc39a8b0d8cf
Jeppe Sigaard <jeppesigaard@Jeppes-MacBook-Pro.local>
2017-04-06 17:51:30 +0200

    Added menu styling and main-menu

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
-
 add_action('init', 'initsettings');
 function initsettings() {
     show_admin_bar( false );
+
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus(array(
+        'main-menu' => 'Hovedmenu',
+    ));
 }
+
+
